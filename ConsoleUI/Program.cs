abc90b8da714c09928178eafeb0621e7b3c1a2bd
Bilal Demirci <dmrc45500@icloud.com>
2024-04-20 17:55:42 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetAllByUnitPrice(100,400))
+{
+    Console.WriteLine(product.ProductName);
+}
+
+Console.ReadLine();
