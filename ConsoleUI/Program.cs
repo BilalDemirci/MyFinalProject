53ebe9e2c678dcde3408a1e8928f5a789f9b5492
Bilal Demirci <dmrc45500@icloud.com>
2024-04-24 16:00:17 +0300

    24.04.2024 - part-2

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,13 +3,12 @@ using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-//ProductTest();
+ProductTest();
+//CategoryTest();
 
-CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-foreach (var category in categoryManager.GetAll())
-{
-    Console.WriteLine(category.CategoryName);
-}
+
+
+
 
 Console.ReadLine();
 
@@ -17,8 +16,17 @@ static void ProductTest()
 {
     ProductManager productManager = new ProductManager(new EfProductDal());
 
-    foreach (var product in productManager.GetAllByUnitPrice(100, 400))
+    foreach (var product in productManager.GetProductDetails())
     {
-        Console.WriteLine(product.ProductName);
+        Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+    }
+}
+
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
     }
 }
